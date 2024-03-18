85d23971b6e4702e29e70c0924c74c269301e6b2
hussein-elmlah <hussein.elmlah7@gmail.com>
2024-03-18 23:23:47 +0200

    use && instead of ternary? operator when no else senario

--- src/reusableComponents/ProjectCard/ProjectCard.jsx
+++ src/reusableComponents/ProjectCard/ProjectCard.jsx
@@ -5,7 +5,7 @@ const ProjectCard = ({ imageSrc, index }) => {
   return (
     <div className="col-md-4">
       <div className="card">
-        {!(index%2) ? <div className="evenProject position-absolute w-100 h-100 shadow-lg d-flex justify-content-start align-items-start ps-2 fw-bold fs-4"><span className='text-info'>EvenProject</span></div> : ''}
+        {!(index%2) && <div className="evenProject position-absolute w-100 h-100 shadow-lg d-flex justify-content-start align-items-start ps-2 fw-bold fs-4"><span className='text-info'>EvenProject</span></div> }
         <img src={imageSrc} alt={`Project ${index + 1}`} />
         <div className="card-description">
           <p>PROJECT {index + 1}</p>
